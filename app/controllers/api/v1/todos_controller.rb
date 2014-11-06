0326bd9d091cce4144ddc2bc827778b40ecd82fd
Noah Holmes <noah.r.holmes@gmail.com>
2014-11-06 09:27:59 -0500

    fixed duplication

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,28 +1,18 @@
 class Api::V1::TodosController < ApplicationController
 	def index
-		@todo = Todo.all
-
-		render json: @todo
+		render json: Todo.all
 	end
 
-	def create
-		@todo = Todo.new(todo_params)
-
-		if @todo.save
-			render json: @todo, status: :created
-		else
-			render @todo.errors, status: :unprocessible_entity
-		end
-	end
+  def create
+    @todo = Todo.new(todo_params)
+		@todo.save
+    render json: [@todo], status: :created
+  end
 
 	def update
 		@todo = Todo.find(params[:id])
-
-		if @todo.update(todo_params)
-			head :no_content
-		else
-			render json: @todo.errors, status: :unprocessable_entity
-		end
+		@todo.update(todo_params)
+		render json: [@todo]
 	end
 
 	def destroy
